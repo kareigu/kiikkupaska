ea90a398fc7e7b0577d422a274f80d9b5181807e
mxr <eetu.kivelae@gmail.com>
2021-09-14 05:17:56 +0300

    Changed to using go-winres for exe-icons

--- dist.bat
+++ dist.bat
@@ -3,7 +3,6 @@ del /S /Q .\dist
 
 go build -ldflags "-s -w -H=windowsgui"
 
-"C:\Program Files (x86)\Resource Hacker\ResourceHacker.exe" -open kiikkupaskaa.exe -save kiikkupaskaa.exe -action addskip -res assets/fav.ico -mask ICONGROUP,MAIN
 
 copy .\kiikkupaskaa.exe .\dist\
 Xcopy /E /I .\assets .\dist\assets
